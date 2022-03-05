0b31b8e317af265b5229bf8b958a4f909e8322ce
user <mininet@cs640vm>
2022-03-05 17:07:11 -0600

    Implemented MACTracker data struct

--- assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -1,15 +1,21 @@
 package edu.wisc.cs.sdn.vnet.sw;
 
 import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.MACAddress;
 import edu.wisc.cs.sdn.vnet.Device;
 import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Aaron Gember-Jacobson
  */
 public class Switch extends Device
 {	
+
+	private MACTracker tracker;
 	/**
 	 * Creates a router for a specific host.
 	 * @param host hostname for the router
@@ -17,6 +23,7 @@ public class Switch extends Device
 	public Switch(String host, DumpFile logfile)
 	{
 		super(host,logfile);
+		tracker = new MACTracker();
 	}
 
 	/**
@@ -31,7 +38,72 @@ public class Switch extends Device
 		
 		/********************************************************************/
 		/* TODO: Handle packets                                             */
+		MACAddress src = etherPacket.getSourceMAC();
+		MACAddress dst = etherPacket.getDestinationMAC();
 		
 		/********************************************************************/
+	}
+
+	/** @author AJ */
+	private class MACTracker
+	{
+		private Timer timer;
+		private ConcurrentHashMap<MACAddress, LiveLink> liveAddresses;
+
+		public MACTracker()
+		{
+			timer = new Timer(true);
+		}
+
+		public Iface getCacheIface(MACAddress macAddr)
+		{
+			LiveLink link = liveAddresses.get(macAddr);
+			if (link == null) return null;
+
+			// reset killTask
+			link.task.cancel();
+			link.task = killEntryTask(macAddr);
+			registerTask(link.task);
+
+			return link.iface;
+		}
+
+		public void put(MACAddress macAddr, Iface iface)
+		{
+			TimerTask task = killEntryTask(macAddr);
+			LiveLink link = new LiveLink(task, iface);
+			liveAddresses.put(macAddr, link);
+			registerTask(task);
+		}
+
+		private void registerTask(TimerTask task)
+		{
+			final int timeout = 15*1000;
+			timer.schedule(task, timeout);
+		}
+
+		private TimerTask killEntryTask(MACAddress macAddr)
+		{
+			return new TimerTask()
+			{
+				public void run()
+				{
+					liveAddresses.remove(macAddr);
+				}
+			};
+		}
+
+		private class LiveLink
+		{
+			public TimerTask task;
+			public final Iface iface;
+
+			public LiveLink(TimerTask task, Iface iface)
+			{
+				this.task = task;
+				this.iface = iface;
+			}
+		}
+
 	}
 }
