2c3b64c5859ce385a870456233f0c9e036f6de50
Sudeep Reddy <sudeepreddy@Sudeeps-MacBook-Pro.local>
2022-03-08 16:50:03 -0600

    changing checksum calc

--- assign2/src/edu/wisc/cs/sdn/vnet/rt/Router.java
+++ assign2/src/edu/wisc/cs/sdn/vnet/rt/Router.java
@@ -97,21 +97,23 @@ public class Router extends Device
 		
 		short orig = packet.getChecksum();
 		packet.setChecksum((short) 0);
-		byte headerLength = packet.getHeaderLength();
-		byte[] data = new byte[packet.getTotalLength()];
-		ByteBuffer bb = ByteBuffer.wrap(data);
-		short sum = (short) 0;
+		// byte headerLength = packet.getHeaderLength();
+		// byte[] data = new byte[packet.getTotalLength()];
+		// ByteBuffer bb = ByteBuffer.wrap(data);
+		packet.serialize();
+		
+		short sum = packet.getChecksum();
 
 		// compute checksum if needed
-		bb.rewind();
-		int accumulation = 0;
-		for (int i = 0; i < headerLength * 2; ++i) {
-			accumulation += 0xffff & bb.getShort();
-		}
-		accumulation = ((accumulation >> 16) & 0xffff)
-				+ (accumulation & 0xffff);
-		sum = (short) (~accumulation & 0xffff);
-		bb.putShort(10, sum);
+		// bb.rewind();
+		// int accumulation = 0;
+		// for (int i = 0; i < headerLength * 2; ++i) {
+		// 	accumulation += 0xffff & bb.getShort();
+		// }
+		// accumulation = ((accumulation >> 16) & 0xffff)
+		// 		+ (accumulation & 0xffff);
+		// sum = (short) (~accumulation & 0xffff);
+		// bb.putShort(10, sum);
 
 		if(orig != sum) {
 
