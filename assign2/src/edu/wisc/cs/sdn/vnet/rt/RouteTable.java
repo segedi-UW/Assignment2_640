6a2f8b27dee6009b75149cfe82c959024b1e3590
Sudeep Reddy <sudeepreddy@Sudeeps-MacBook-Pro.local>
2022-03-08 17:23:48 -0600

    print changes

--- assign2/src/edu/wisc/cs/sdn/vnet/rt/RouteTable.java
+++ assign2/src/edu/wisc/cs/sdn/vnet/rt/RouteTable.java
@@ -48,7 +48,7 @@ public class RouteTable
 				int mask = entry.getMaskAddress();
 				// and the mask with the ip
 				int andResult = mask & ip;
-				System.out.printf("Gateway: %d , AND: %d\n", entry.getGatewayAddress(), andResult);
+				System.out.printf("Destination: %d , AND: %d\n", entry.getDestinationAddress(), andResult);
 				if (andResult == entry.getDestinationAddress()) {
 					// System.out.println("Matched");
 					if (andResult > longestMask) {
